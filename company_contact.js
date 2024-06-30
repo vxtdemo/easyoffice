5a19032a7717a4750d12b115d7ee0ed991b8054a
vxtdemo <60476736+vxtdemo@users.noreply.github.com>
2024-06-30 16:09:09 +0700

    Add files via upload

---
+++ company_contact.js
@@ -0,0 +1,6 @@
+sCompanyContact  = '<b>CÔNG TY CỔ PHẦN VIKING VIỆT NAM</b>';
+sCompanyContact += '<p>Nhà máy: Km 5, quốc lộ 10, xã Thành Lợi, huyện Vụ Bản, tỉnh Nam Định';
+sCompanyContact += '<br>Hotline: Mr. Hùng 0982-312-373, Email: hung@vikingjsc.com';
+sCompanyContact += '<br>Số ĐKKD: 0106 610 502 do Sở KHĐT Hà Nội cấp ngày 30/07/2014';
+
+document.getElementById("company_contact").innerHTML = sCompanyContact;
--- web_footer.js
+++ web_footer.js
@@ -1,1 +1,31 @@
-document.getElementById("web_footer").innerHTML = '<table border=0 width="100%"><tr><td>© 2020 by Viking JSC.<td align="right"><small><small><small>Designed by "Van Xuan Technology Service Company Limited | http://vxtgroup.com" | v1vi QN </table>';
+function menu_top_toggle(myObject) 
+{
+  menu_button_change(myObject);
+
+  var x = document.getElementById("menu_top");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function menu_level_2_toggle(sMenuLevel2Name, sExpandCollapseItem)
+{
+  var x = document.getElementById(sMenuLevel2Name);
+  var ec = document.getElementById(sExpandCollapseItem);
+
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    ec.className = "down" ;
+  } else {
+    x.style.display = "block";
+    ec.className = "up" ;
+  }
+}
+
+function menu_button_change(x) {
+  x.classList.toggle("change");
+}
+
+document.getElementById("web_footer").innerHTML = '<table border=0 width="100%"><tr><td>© 2023 by Viking JSC.<td align="right"><small><small><a href="https://vxtgroup.com/contact" style="text-decoration: none">Thiết kế bởi Công ty Vạn Xuân (https://vxtgroup.com/contact)</a> | v2305 </table>';
